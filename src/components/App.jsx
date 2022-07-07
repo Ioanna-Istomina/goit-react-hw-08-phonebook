67b19cfcb84a65768ac4ad80cebf757caf9cb4d4
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-07-07 11:42:20 +0300

    fix bag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ export class App extends Component {
   };
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevState !== this.state.contacts) {
+    if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
